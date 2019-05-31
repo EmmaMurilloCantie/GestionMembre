6017c37ae7540c7ce0b9c4be75197a9463b5fc1e
emma <emma@macbook-de-murillo.home>
2019-05-31 16:11:12 +0200

    TestGit

---
+++ src/main/java/com/example/GestionMembre/Repositories/RepoEnMarche.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.GestionMembre.Repositories;
+
+import com.example.GestionMembre.Entities.EnMarche;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author emma
+ */
+public interface RepoEnMarche extends CrudRepository<EnMarche, Long> {
+    
+}
